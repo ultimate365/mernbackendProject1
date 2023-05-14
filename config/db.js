da967fd3834a5e13e74a76840a70c399d103a285
ultimate365 <34962965+ultimate365@users.noreply.github.com>
2023-05-14 19:42:55 +0530

    Updated

--- config/db.js
+++ config/db.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.HOST}:${process.env.DBPORT}/${process.env.DATABASE}`)
+var uri = `mongodb://maidul365:${process.env.PASSWORD}@ac-6spwm0o-shard-00-00.julotqr.mongodb.net:27017,ac-6spwm0o-shard-00-01.julotqr.mongodb.net:27017,ac-6spwm0o-shard-00-02.julotqr.mongodb.net:27017/${process.env.DATABASE}?ssl=true&replicaSet=atlas-4uf94d-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(con => console.log('Connected To DB'))
     .catch(err => console.log('error', err))
 
 
 module.exports = mongoose;
+
+
